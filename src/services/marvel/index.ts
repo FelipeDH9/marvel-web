a421a781b57fffbce07cf206183a02d249085a7c
Felipe <felipehaddad25@gmail.com>
2022-05-30 11:06:26 -0300

    feat: project creation and create marvel services

---
+++ src/services/marvel/index.ts
@@ -0,0 +1,55 @@
+import axios,{Axios, Method} from 'axios'
+import md5 from 'js-md5'
+import {GetComicsByIdSchema, GetCharactersSchema, GetCharactersResponse, GetCharactersBtIdResponse, GetComicsByIdResponse} from './schema'
+
+const doRequest = async ({ method = 'GET', body, headers={}, params, path }: {method: Method, body?: any, headers?: {}, params?: any, path: string}) => {
+  try {
+    const timestamp = new Date().getTime();
+    const hash = md5(timestamp + String(process.env.REACT_APP_PRIVATE_KEY) + String(process.env.REACT_APP_PUBLIC_KEY));
+    const customParams = {
+      hash,
+      ts: timestamp,
+      apikey: String(process.env.REACT_APP_PUBLIC_KEY),
+    };
+
+    const {data}= await axios.request({
+      method,
+      params: {...params, customParams},
+      url: `https://developer.marvel.com/v1/public${path}`,
+      data: body,
+      headers
+    })
+    return data
+  } catch (error) {
+    console.log({ error })
+    return error
+  }
+}
+
+export const getCharacters = async (params?:GetCharactersSchema) => {
+  const hasParams = params ? { ...params } : {}
+  const characters = await doRequest({
+    method: 'GET',
+    path: '/characters',
+    params: hasParams
+  })
+  return characters as GetCharactersResponse
+}
+
+export const getCharactersById = async (characterId: number) =>{
+  const character = await doRequest({
+    method: 'GET',
+    path: `/characters/${characterId}`
+  })
+  return character as GetCharactersBtIdResponse
+}
+
+export const getComicsById = async ({characterId, params}:{characterId: number, params?: GetComicsByIdSchema}) =>{
+  const hasParams = params ? { ...params } : {}
+  const comics = await doRequest({
+    method:'GET',
+    path: `/characters/${characterId}/comics`,
+    params: hasParams
+  })
+  return comics as GetComicsByIdResponse
+}
---
+++ src/services/marvel/schema/index.ts
@@ -0,0 +1,320 @@
+export type GetComicsByIdSchema = {
+  format?: string; 
+  formatType?: string; 
+  noVariants?: boolean; 
+  dateDescriptor?: string; 
+  dateRange?: number; 
+  title?:string; 
+  titleStartsWith?:string; 
+  startYear?: number; 
+  issueNumber?: number; 
+}
+
+export type GetCharactersSchema = {
+  name?:string, 
+  nameStartsWith?: string, 
+  modifiedSince?: Date, 
+  comics?: number, 
+  series?: number, 
+  events?: number, 
+  stories?: number, 
+  orderBy?: string, 
+  limit?: number, 
+  offset?: number
+}
+
+export type GetCharactersResponse = {
+  code: number,
+  status: string,
+  copyright: string,
+  attributionText: string,
+  attributionHTML: string,
+  data: {
+    offset: number,
+    limit: number,
+    total: number,
+    count: number,
+    results: [
+      {
+        id: number,
+        name: string,
+        description: string,
+        modified: Date,
+        resourceURI: string,
+        urls: [
+          {
+            type: string,
+            url: string
+          }
+        ],
+        thumbnail: {
+          path: string,
+          extension: string
+        },
+        comics: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        },
+        stories: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string,
+              type: string
+            }
+          ]
+        },
+        events: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        },
+        series: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        }
+      }
+    ]
+  },
+  etag: string
+}
+
+export type GetCharactersBtIdResponse = {
+  code: number,
+  status: string,
+  copyright: string,
+  attributionText: string,
+  attributionHTML: string,
+  data: {
+    offset: number,
+    limit: number,
+    total: number,
+    count: number,
+    results: [
+      {
+        id: number,
+        name: string,
+        description: string,
+        modified: Date,
+        resourceURI: string,
+        urls: [
+          {
+            type: string,
+            url: string
+          }
+        ],
+        thumbnail: {
+          path: string,
+          extension: string
+        },
+        comics: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        },
+        stories: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string,
+              type: string
+            }
+          ]
+        },
+        events: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        },
+        series: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        }
+      }
+    ]
+  },
+  etag: string
+}
+
+export type GetComicsByIdResponse = {
+  code: number,
+  status: string,
+  copyright: string,
+  attributionText: string,
+  attributionHTML: string,
+  data: {
+    offset: number,
+    limit: number,
+    total: number,
+    count: number,
+    results: [
+      {
+        id: number,
+        digitalId: number,
+        title: string,
+        issueNumber: number,
+        variantDescription: string,
+        description: string,
+        modified: Date,
+        isbn: string,
+        upc: string,
+        diamondCode: string,
+        ean: string,
+        issn: string,
+        format: string,
+        pageCount: number,
+        textObjects: [
+          {
+            type: string,
+            language: string,
+            text: string
+          }
+        ],
+        resourceURI: string,
+        urls: [
+          {
+            type: string,
+            url: string
+          }
+        ],
+        series: {
+          resourceURI: string,
+          name: string
+        },
+        variants: [
+          {
+            resourceURI: string,
+            name: string
+          }
+        ],
+        collections: [
+          {
+            resourceURI: string,
+            name: string
+          }
+        ],
+        collectedIssues: [
+          {
+            resourceURI: string,
+            name: string
+          }
+        ],
+        dates: [
+          {
+            type: string,
+            date: Date
+          }
+        ],
+        prices: [
+          {
+            type: string,
+            price: number
+          }
+        ],
+        thumbnail: {
+          path: string,
+          extension: string
+        },
+        images: [
+          {
+            path: string,
+            extension: string
+          }
+        ],
+        creators: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string,
+              role: string
+            }
+          ]
+        },
+        characters: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string,
+              role: string
+            }
+          ]
+        },
+        stories: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string,
+              type: string
+            }
+          ]
+        },
+        events: {
+          available: number,
+          returned: number,
+          collectionURI: string,
+          items: [
+            {
+              resourceURI: string,
+              name: string
+            }
+          ]
+        }
+      }
+    ]
+  },
+  etag: string
+}
